380cc4caaea5f6f8bcca30f9ec438c70b1b84612
rreyes5132 <151219839+rreyes5132@users.noreply.github.com>
2023-12-01 10:45:20 +0100

    Update Coco.java again

--- src/rorelo/Coco.java
+++ src/rorelo/Coco.java
@@ -6,7 +6,7 @@ public class Coco {
 		// TODO Auto-generated method stub
 		System.out.println("Rocío");
 		System.out.println("Coco");
-		System.out.println("chanel");
+		System.out.println("Chanel");
 	}
 
 }
